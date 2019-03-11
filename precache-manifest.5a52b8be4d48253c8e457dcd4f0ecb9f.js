c94d2b5306a9b36834ede7a855a8f336c15f21a6
David Kim <rtdk0324@gmail.com>
2019-03-11 16:30:18 -0400

    Updates

---
+++ precache-manifest.5a52b8be4d48253c8e457dcd4f0ecb9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0bf993e288663187f7c87aa9d90c9ae4",
+    "url": "/movies/static/media/spinner.0bf993e2.gif"
+  },
+  {
+    "revision": "04129ba559ae92aa086e",
+    "url": "/movies/static/js/runtime~main.712e9881.js"
+  },
+  {
+    "revision": "010d4326459813e08c58",
+    "url": "/movies/static/js/main.02f1f094.chunk.js"
+  },
+  {
+    "revision": "ae7ff21a3e0be93514da",
+    "url": "/movies/static/js/2.607fe365.chunk.js"
+  },
+  {
+    "revision": "010d4326459813e08c58",
+    "url": "/movies/static/css/main.dac41f42.chunk.css"
+  },
+  {
+    "revision": "0138e60b5a6ae3a5cdcbc1a570ec320e",
+    "url": "/movies/index.html"
+  }
+];
